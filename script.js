5e2af32ae2426f59f274fb4cc3daa63e8d319452
Jade Quinn <jadekquinn@gmail.com>
2021-04-01 15:30:30 +0000

    (feat): Add empty functions to script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateRightAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
