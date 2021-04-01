bd06e5757c68f6e061c68ce1f992f5f03ef50413
Jade Quinn <jadekquinn@gmail.com>
2021-04-01 15:44:48 +0000

    (feat): Add event listeners to script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+// The main game "loop", called when the script is first loaded
+// and after the user's answer has been processed
+
 function runGame() {
 
 }
+
+// Called when the user clicks the Submit button or presses Enter
 
 function checkAnswer() {
 
@@ -17,6 +37,8 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
+
+// Displays the questions.
 
 function displayAdditionQuestion() {
 
